5a5550bdcf0a42533e1f6df49b08f19cd367eae9
kuma88 <kumarasond@gmail.com>
2020-12-15 10:07:49 +0800

    refactored busstop and bus number to variable

--- App.js
+++ App.js
@@ -1,33 +1,52 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, ActivityIndicator } from "react-native";
 
-const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=84231"
+const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=" + BUSSTOP_NUMBER;
+const BUSSTOP_NUMBER = "84231";
+const LOADING_INTERVAL = 60000;
+const BUS_NUMBER = "14";
+
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [arrival, setArrival] = useState("");
 
-  useEffect(() => {
+  function loadBusStopData(){
+    setLoading(true);
+
     fetch(BUSSTOP_URL)
       .then((response) => response.json())
-      .then((responseData) => responseData);
+      .then((responseData) => {
+        const myBus = responseData.services.filter(
+          (item) => item.no === BUS_NUMBER
+          )[0];
 
-      const myBus = responseData.services.filter(
-        (item) => item.no === "14"
-      )[0];
-      console.log("My Bus:");
-      console.log(myBus);
+          //console.log(myBus);
+          const duration_s = Math.floor(myBus.next.duration_ms / 1000)
+          const minutes = Math.floor(duration_s / 60)
+          const seconds = duration_s % 60
+          
+          setArrival(`${minutes} minutes and ${seconds} seconds`);
+          setLoading(false);
+      });
+  }
 
+  useEffect(() => {
+    const interval = setInterval(loadBusStopData, LOADING_INTERVAL);
+    loadBusStopData();
+
+    return () => clearInterval(interval);
   }, []);
 
 
- return (
+  return (
    <View style={styles.container}>
      <Text style={styles.title}>Bus arrival time:</Text>
      <Text style={styles.arrivalTime}>
-       {loading? <ActivityIndicator style={styles.horizontal} size="large" color="#00ff00" />: "Loaded"}
+       {loading? <ActivityIndicator style={styles.horizontal} size="large" color="#00ff00" />: arrival }
      </Text>
       
-     <TouchableOpacity style={styles.button}>
+     <TouchableOpacity onPress={loadBusStopData} style={styles.button}>
        <Text style={styles.buttonText}>Refresh!</Text>
      </TouchableOpacity>
    </View>
@@ -49,6 +68,7 @@ const styles = StyleSheet.create({
  arrivalTime: {
    fontSize: 64,
    marginBottom: 32,
+   textAlign: "center"
  },
  button: {
    padding: 20,
